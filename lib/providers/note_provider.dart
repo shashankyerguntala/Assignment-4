5f3189d6a5f1f6510f1487c0b2c9beb31ae46644
shashankyerguntala <93209763+shashankyerguntala@users.noreply.github.com>
2025-08-04 12:15:16 +0530

    Update note_provider.dart

--- lib/providers/note_provider.dart
+++ lib/providers/note_provider.dart
@@ -4,24 +4,6 @@ import 'package:note_app/models/note_model.dart';
 class NoteProvider extends ChangeNotifier {
   List<NoteModel> notesList = [];
 
-  bool _isSearching = false;
-  String _searchQuery = '';
-
-  bool get isSearching => _isSearching;
-  String get searchQuery => _searchQuery;
-
-  List<NoteModel> get filteredNotes {
-    if (_searchQuery.isEmpty) {
-      return [];
-    }
-    return notesList
-        .where(
-          (note) =>
-              note.title.toLowerCase().contains(_searchQuery.toLowerCase()),
-        )
-        .toList();
-  }
-
   void addNote(NoteModel note) {
     notesList.add(note);
     notifyListeners();
@@ -38,21 +20,5 @@ class NoteProvider extends ChangeNotifier {
       notesList[index] = newNote;
       notifyListeners();
     }
-  }
-
-  void setSearchQuery(String query) {
-    _searchQuery = query;
-    notifyListeners();
-  }
-
-  void startSearch() {
-    _isSearching = true;
-    notifyListeners();
-  }
-
-  void stopSearch() {
-    _isSearching = false;
-    _searchQuery = '';
-    notifyListeners();
   }
 }
